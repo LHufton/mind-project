7d846c761d57ad3ee6c0406ac013c4e9fdbf7f9e
Luke Hufton <lukehufton@gmail.com>
2023-05-30 19:24:44 -0500

    11

--- mind-project/routes/habits.js
+++ mind-project/routes/habits.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const habitsCtrl = require('../controllers/habits')
+router.get('/', habitsCtrl.index)
+router.get('/new', habitsCtrl.new)
 
-module.exports = router;
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+module.exports = router
