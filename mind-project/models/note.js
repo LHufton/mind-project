a9ab8b3ee10dcad47621fcd01cda3ab8ad7fe4a2
Luke Hufton <lukehufton@gmail.com>
2023-06-01 01:25:35 -0500

    updated models

--- mind-project/models/note.js
+++ mind-project/models/note.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const noteSchema = new Schema({
-  comment: String,
-  required: true
+  comment: {
+    type: String,
+    required: true
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
